dae03977eddf3d350fed785faa959cea39bc5e9e
Анастасия Лемешко <an.zaschepenkova@yandex.ru>
2024-07-18 07:39:32 +0300

    попытка вынести обработчик

--- js/full-photo-modal.js
+++ js/full-photo-modal.js
@@ -1,5 +1,7 @@
 import {isEscapeKey, isEnterKey} from './utils.js';
+import {renderFullSizePhoto} from './render-full-size-photo.js';
 
+const photoList = document.querySelectorAll('.picture');
 const fullSizePhoto = document.querySelector('.big-picture');
 const closeFullSizePhoto = fullSizePhoto.querySelector('.cancel');
 
@@ -23,6 +25,12 @@ function closeUserModal () {
 }
 
 //тут должен быть обработчик на клик по миниатюре (открытие большого фото), но он лежит в render-full-size-photo
+
+// photoList.forEach((photo) => {
+//   openUserModal ();
+//   photo.addEventListener('click', renderFullSizePhoto(photoList));
+
+// })
 
 closeFullSizePhoto.addEventListener('click', () => {
   closeUserModal();
