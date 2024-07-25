dfe2437b3998ea9373481117a4fadfc0518da0c2
Анастасия Лемешко <an.zaschepenkova@yandex.ru>
2024-07-25 22:44:46 +0300

    рефакторинг изменяющего масштаб кода

--- js/img-scale-change.js
+++ js/img-scale-change.js
@@ -10,35 +10,44 @@ const scaleBigger = document.querySelector('.scale__control--bigger');
 const scaleValue = document.querySelector('.scale__control--value');
 const imgPreview = document.querySelector('.img-upload__preview img');
 
-scaleSmaller.addEventListener('click', () => {
-  let scaleNumber = parseInt(scaleValue.value, 10);
-  if (scaleNumber > SCALE_MIN) {
-    scaleBigger.disabled = false;
-    scaleNumber -= SCALE_STEP;
-    scaleValue.value = scaleNumber + '%';
-    scaleNumber /= 100;
-    imgPreview.style.transform = `scale(${scaleNumber})`;
-  } else {
+const isScaleButtonDisabled = () => {
+  scaleSmaller.disabled = false;
+  scaleBigger.disabled = false;
+
+  if (scaleNumber <= SCALE_MIN) {
     scaleSmaller.disabled = true;
+  }
+
+  if (scaleNumber >= SCALE_MAX) {
+    scaleBigger.disabled = true;
+  }
+};
+
+let scaleNumber = parseInt(scaleValue.value, 10);
+
+const setStyle = (scale) => {
+  scaleValue.value = scale + '%';
+  imgPreview.style.transform = `scale(${scale/100})`;
+};
+
+scaleSmaller.addEventListener('click', () => {
+  if (scaleNumber > SCALE_MIN) {
+    scaleNumber -= SCALE_STEP;
+    setStyle(scaleNumber);
+    isScaleButtonDisabled();
   }
 });
 
 scaleBigger.addEventListener('click', () => {
-  let scaleNumber = parseInt(scaleValue.value, 10);
   if (scaleNumber < SCALE_MAX) {
-    scaleSmaller.disabled = false;
     scaleNumber += SCALE_STEP;
-    scaleValue.value = scaleNumber + '%';
-    scaleNumber /= 100;
-    imgPreview.style.transform = `scale(${scaleNumber})`;
-  } else {
-    scaleBigger.disabled = true;
+    setStyle(scaleNumber);
+    isScaleButtonDisabled();
   }
 });
 
 const resetScale = () => {
-  scaleValue.value = SCALE_DEFAULT + '%';
-  imgPreview.style.transform = `scale(${SCALE_DEFAULT/100})`
+  setStyle(SCALE_DEFAULT);
 };
 
 export {resetScale};
